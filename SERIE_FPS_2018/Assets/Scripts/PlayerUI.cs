a0801408cb4d3080d397f70e2c6bac251f475dc0
ProjectDronner <43302157+ProjectDronner@users.noreply.github.com>
2018-09-16 17:13:28 +0200

    Initial commit

---
+++ SERIE_FPS_2018/Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerUI : MonoBehaviour {
+
+    [SerializeField]
+    private RectTransform thrusterFuelFill;
+
+    private PlayerController controller;
+
+    [SerializeField]
+    private GameObject pauseMenu;
+
+    [SerializeField]
+    private GameObject scoreboard;
+
+    private void Start()
+    {
+        PauseMenu.isOn = false;
+    }
+
+    public void SetController(PlayerController _controller)
+    {
+        controller = _controller;
+    }
+
+    private void Update()
+    {
+        SetFuelAmount(controller.GetThrusterFuelAmount());
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            scoreboard.SetActive(true);
+        }else if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            scoreboard.SetActive(false);
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        PauseMenu.isOn = pauseMenu.activeSelf;
+    }
+
+    void SetFuelAmount(float _amount)
+    {
+        thrusterFuelFill.localScale = new Vector3(1f, _amount, 1f);
+    }
+}
